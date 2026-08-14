324339fa9719890e55a0127f5402e0b8aac6fe4c
Eggchese <tunglamtrannguyen3@gmail.com>
2026-08-14 14:23:10 +0700

    Re-export serial module from x86_64

--- src/arch/mod.rs
+++ src/arch/mod.rs
@@ -1,7 +1,7 @@
 // src/arch/mod.rs
 
 pub mod serial;
-
+pub use x86_64::serial;
 /// Low-level x86 port output instruction
 pub unsafe fn outb(port: u16, val: u8) {
     core::arch::asm!("out dx, al", in("dx") port, in("al") val);
