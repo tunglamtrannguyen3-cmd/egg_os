8c904280fe85d8a6a0f3482be1a6ab4ea40819e5
Eggchese <tunglamtrannguyen3@gmail.com>
2026-08-15 13:16:39 +0700

    Add HostMemoryManager and HOST_PHYSICAL_ALLOCATOR to exports

--- host_kernel/src/memory/mod.rs
+++ host_kernel/src/memory/mod.rs
@@ -1,3 +1,6 @@
 pub mod ept;
 
-pub use ept::{EptEntry, EptFlags, EptPageTable};
+pub use ept::{
+    init_host_memory, EptEntry, EptFlags, EptPageTable, HostMemoryManager,
+    HOST_PHYSICAL_ALLOCATOR,
+};
