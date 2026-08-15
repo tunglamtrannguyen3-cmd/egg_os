da481a5b4c37088aa207987e50186bb2242f22db
Eggchese <tunglamtrannguyen3@gmail.com>
2026-08-15 13:46:25 +0700

    Remove aarch64 linker and boot file notifications
    Removed linker and boot file change notifications for aarch64 architecture.

--- security_kernel/build.rs
+++ security_kernel/build.rs
@@ -1,7 +1,8 @@
-fn main() {
-    println!("cargo:rerun-if-changed=linker_x86_64.ld");
-    println!("cargo:rerun-if-changed=linker_aarch64.ld");
-    println!("cargo:rerun-if-changed=src/arch/x86_64/boot.s");
-    println!("cargo:rerun-if-changed=src/arch/aarch64/boot.s");
-}
+use std::env;
+use std::path::PathBuf;
 
+fn main() {
+    let manifest_dir = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap());
+    println!("cargo:rustc-link-search={}", manifest_dir.display());
+    println!("cargo:rerun-if-changed=linker_x86_64.ld");
+}
